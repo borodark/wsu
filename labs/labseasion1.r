2bf3d73d0b988ac39b2bd08b5fcb0434bf25b9d6
Igor O'Sten <borodark@gmail.com>
2019-02-18 14:25:30 -0500

    Project started

---
+++ labs/labseasion1.r
@@ -0,0 +1,116 @@
+#install.packages()
+library (MASS)
+library (ISLR)
+library(ggplot2)
+######################################
+# Vector and Matrtix
+x <- c(1,3,2,5)
+x
+y = c(1,4,3,6)
+length (x)
+length (y)
+x+y
+x=rnorm (50)
+y=x+rnorm (50, mean=50, sd=.1)
+cor(x,y)
+set.seed (1303)
+rnorm (50)
+set.seed (3)
+y=rnorm (100)
+mean(y)
+var(y)
+sqrt(var(y))
+sd(y)
+
+x=matrix (data=c(1,2,3,4) , nrow=2, ncol =2)
+x
+x=matrix (c(1,2,3,4) ,2,2)
+x
+matrix (c(1,2,3,4) ,2,2,byrow =TRUE)
+sqrt(x)
+x^2
+
+# Indexing
+
+A=matrix (1:16 ,4 ,4)
+A
+A[2,3]
+A[c(1,3) ,c(2,4) ]
+A[1:3 , 2:4]
+A[1:2 ,]
+A[ ,1:2]
+A[-c(1,3) ,]
+dim(A)
+
+
+# Graphic
+
+x=rnorm (100)
+y=rnorm (100)
+plot(x,y)
+plot(x,y,xlab=" this is the x-axis",ylab=" this is the y-axis", main=" Plot of X vs Y")
+
+# Loading Data
+
+Auto=read.table ("Auto.data")
+fix(Auto)
+Auto=read.table ("Auto.data", header =T,na.strings ="?")
+fix(Auto)
+Auto=read.csv ("Auto.csv", header =T,na.strings ="?")
+dim(Auto)
+Auto=na.omit(Auto)
+dim(Auto)
+names(Auto)
+pairs(Auto)
+pairs(~ mpg + displacement + horsepower + weight + acceleration , Auto)
+plot(Auto$horsepower ,Auto$mpg)
+identify (Auto$horsepower ,Auto$mpg ,Auto$name)
+summary (Auto)
+
+# Linear Regression
+data("Boston")
+fix("Boston")
+names(Boston)
+lm.fit =lm(medv~lstat ,data=Boston )
+attach (Boston )
+lm.fit =lm(medv~lstat)
+lm.fit
+names(lm.fit )
+coef(lm.fit)
+confint (lm.fit)
+
+predict (lm.fit ,data.frame(lstat=c(5 ,10 ,15, 20) ), interval ="confidence")
+plot(lstat, age)
+abline (lm.fit)
+ggplot(data = Boston, mapping = aes(x = lstat, y = medv)) + geom_point() + geom_smooth(method = "lm")
+par(mfrow=c(2,2))
+plot(lm.fit)
+
+lm.fit =lm(medv~lstat+age ,data=Boston )
+summary (lm.fit)
+
+summary(lm.fit)$r.sq
+# Non-linear regession
+
+lm.fit =lm(medv~lstat*age,data=Boston )
+lm.fit =lm(medv~.,data=Boston )
+
+lm.fit2=lm(medv~age+I(lstat ^2))
+lm.fit =lm(medv~lstat)
+anova(lm.fit ,lm.fit2)
+lm.fit5=lm(medv~poly(age ,5))
+summary (lm.fit5)
+summary (lm(medv~log(rm),data=Boston ))
+
+library (car)
+#vif(lm.fit)
+#	Qualitative Predictors
+Carseats
+fix(Carseats)
+names(Carseats)
+lm.fit =lm(Sales~.+ Income :Advertising +Price :Age ,data=Carseats )
+summary (lm.fit)
+attach (Carseats )
+contrasts (ShelveLoc)
+
+
---
+++ project/README.md
@@ -0,0 +1,11 @@
+# Time Series Analysis for Log Returns
+
+The project is intend to analyze log return of several market instruments. The main goal is to estimate how to aprach the task of finding if some insruments log Returns are correlated. After aquiring datasets and transforming to obtain the log return for daily time series we'll study several market instruments: indexes, currencies. We assess time series frequency parameters and detect Trend and Cycles by Band Pass Filter. We analize ACF to recommend the ARIMA and possibly SARIMA, if series exhibits seasonality, model parameters. We assess and compare model accuracies. Moving to Vector ARMA Models we try asses the efford to model several unit-root nonstationar time series jointly to find co-integration.
+
+
+## Literature
+
+* Time Series Analysis and Its Applications With R Examples by Robert H. Shumway David S. Stoffer
+* Analysis of Financial Time Series Financial Econometrics by RUEY S. TSAY University of Chicago
+* [Time series Forecasting — ARIMA models](https://towardsdatascience.com/time-series-forecasting-arima-models-7f221e9eee06)
+* [Introduction To Time Series Forecasting](https://www.datascience.com/blog/introduction-to-forecasting-with-arima-in-r-learn-data-science-tutorials)
