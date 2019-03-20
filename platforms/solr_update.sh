3f04063f00d457e16edcd8729fbf91ad2126768b
Igor O'Sten <borodark@gmail.com>
2019-03-20 16:49:25 -0400

    TOC works

--- platforms/solr_update.sh
+++ platforms/solr_update.sh
@@ -1,4 +1,4 @@
-curl -v -k 'https://35.224.130.80/solr/wsu/update/json/docs?commit=true&split=/|/table_of_contents&f=table_of_contents:/table_of_contents&f=isbn_10:/isbn_10&f=title:/title&f=publishers:/publishers&f=description:/description&f=by_statement:/by_statement&f=subjects:/subjects' \
+curl -v -k 'https://35.224.130.80/solr/wsu/update/json/docs?commit=true&split=/|/table_of_contents/title&f=table_of_contents.title:/table_of_contents/title&f=isbn_10:/isbn_10&f=title:/title&f=publishers:/publishers&f=description:/description&f=by_statement:/by_statement&f=subjects:/subjects' \
      -H 'Content-type:application/json' \
      -d @$1 \
      -u ${SOLR_CREDS}
