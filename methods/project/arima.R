a0d6257273cd50ef70f2fd438ec97aea3f6635b2
Igor O'Sten <borodark@gmail.com>
2019-04-22 23:44:50 -0400

    AAL sorted, Date and Close only in the frame

--- methods/project/arima.R
+++ methods/project/arima.R
@@ -2,31 +2,47 @@ ar_ret <- function(P) {c(NA, P[2:length(P)]/P[1:(length(P)-1)] -1)}
 log_ret <- function(P) {c(NA, log(P[2:length(P)]/P[1:(length(P)-1)]))}
 airlaines.file <- 'data/stock_market_data-AAL.csv'
 rel.file <- 'data/RELIANCE.csv'
-df.aal <- read.csv(airlaines.file, colClasses = c('numeric','Date','numeric','numeric','numeric','numeric'))
+
+df._aal <- read.csv(airlaines.file, colClasses = c('numeric','Date','numeric','numeric','numeric','numeric'))
 df.rel <- read.csv(rel.file, colClasses = c('Date','numeric','numeric','numeric','numeric','numeric','numeric'))
+# AAL sorted backwards
+library(dplyr)
+
+
+df._aal <- df._aal %>% select(Date, Close)
+df.aal <- df._aal[order(df._aal$Date),]
+
 # AAL returns
 df.aal$ar_ret <- ar_ret(df.aal$Close)
 df.aal$log_ret <- log_ret(df.aal$Close)
 # na.omit(df.aal$ret)
 library(tseries)
+# https://en.wikipedia.org/wiki/Augmented_Dickey%E2%80%93Fuller_test
 # ADF test on ariphmetic return
-print(adf.test(na.omit(df.aal$ret)))
-#
-print(adf.test(na.omit(df.aal$ret)))
+print(adf.test(na.omit(df.aal$ar_ret)))
+# ADF test on log return
+print(adf.test(na.omit(df.aal$log_ret)))
 # ADF test on Close
 print(adf.test(na.omit(df.aal$Close)))
-# ADF test on Open
-print(adf.test(na.omit(df.aal$Open)))
 # Realiance returns
-df.rel$ret <- returns(df.rel$Close)
+df.rel$ar_ret <- ar_ret(df.rel$Close)
+df.rel$log_ret <- log_ret(df.rel$Close)
 # na.omit(df.aal$ret)
 # ADF test on return
-print(adf.test(na.omit(df.rel$ret)))
+print(adf.test(na.omit(df.rel$ar_ret)))
+print(adf.test(na.omit(df.rel$log_ret)))
 # ADF test on Close
 print(adf.test(na.omit(df.rel$Close)))
-# ADF test on Open
-print(adf.test(na.omit(df.rel$Open)))
 #
+library(stats)
+aal.arima <- arima(x = df.aal$ar_ret, order = c(2,0,2) )
+print(coef(aal.arima))
+print(summary(aal.arima))
 library(forecast)
 
-aal.arima
+aal.ar.autoarima <- auto.arima(df.aal$ar_ret,max.p = 30, max.q = 30, max.d = 3, trace = TRUE)
+print(aal.ar.autoarima)
+
+rel.ar.autoarima <- auto.arima(df.rel$ar_ret,max.order=30, trace = TRUE)
+print(rel.ar.autoarima)
+
