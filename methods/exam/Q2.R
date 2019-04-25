48dec800687a5bd50c0f2a7e06659df20e0b659d
Igor O'Sten <borodark@gmail.com>
2019-04-25 18:43:35 -0400

    one to go

--- methods/exam/Q2.R
+++ methods/exam/Q2.R
@@ -1,9 +1,12 @@
-x <- matrix(c(1.5,3.2,5.4,8.5))
-y <- c(1,1,-1,-1)
-train <- data.frame(x=x)
-test <- data.frame(x = matrix(c(6.2)))
 library (class)
-knn(train=train ,test =test, cl=as.factor(y), k=3)
-
+x <- as.matrix(c(1.5,3.2,5.4,6.2,8.5))
+y <- as.matrix(c(1,1,-1,-1,-1))
+train <- data.frame(x=x)
+test <- data.frame(x = matrix(c(3.84,3.85)))
+knn3 <-knn(train=train ,test =test, cl=as.factor(y), k=3, prob=TRUE)
+print(knn3)
+attributes(knn3)
+table(knn3,test)
+summary(knn3)
 
 
