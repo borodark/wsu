daa9c3f6b4b3a78ea1d9877880e46818659dfb84
Igor O'Sten <borodark@gmail.com>
2019-04-27 16:50:03 -0400

    pushing project

--- methods/project/README.md
+++ methods/project/README.md
@@ -1,5 +1,8 @@
 # Time Series Analisys
 This is the report on the course final project for DSA 6100: Statistical Methods for Data Science and Analytics
+By 
+* Igor Ostaptchenko <igor_ost@wayne.edu>
+* Ali Milhem <gu...@wayne.edu>
 
 ## Time Series Analisys
 
@@ -17,16 +20,131 @@ This is the report on the course final project for DSA 6100: Statistical Methods
 
 ### Stationarity
 
-#### AAL
+Let's asses the Stationarity of AAL and REL `Close` price values 
+
+#### The Augmented Dickey-Fuller Test 
+For the daily `Close` prices of AAL time series :
+
+```
+> print(adf.test(df.aal$Close,alternative = "stationary"))
+
+	Augmented Dickey-Fuller Test
+
+data:  df.aal$Close
+Dickey-Fuller = -1.4708, Lag order = 15, p-value = 0.8024
+alternative hypothesis: stationary
+```
+
+For the daily `Close` prices of REL time series :
+
+```
+> print(adf.test(df.rel$Close,alternative = "stationary"))
+
+	Augmented Dickey-Fuller Test
+
+data:  df.rel$Close
+Dickey-Fuller = -1.3671, Lag order = 17, p-value = 0.8464
+alternative hypothesis: stationary
+```
+
+TODO Ali - write up the conclusion and motivation to do fitting on Returns instead 
+
+
+### Moving focus to Arithmetic and Log Returns
+TODO Ali - write up the rules and formulas for returns 
+
+#### Transform `Close` prices to `Returns`
+The `R` functions to calculate returns are as follows:
+
+```r
+ar_ret <- function(P) {c(NA, P[2:length(P)]/P[1:(length(P)-1)] -1)}
+log_ret <- function(P) {c(NA, log(P[2:length(P)]/P[1:(length(P)-1)]))}
+```
+
+These functions will be applyed to the `Close` price vector and create two more columns `ar_ret` and `log_ret`
+
+```
+> df.aal$ar_ret <- ar_ret(df.aal$Close)
+> df.aal$log_ret <- log_ret(df.aal$Close)
+> head(df.aal)
+        Date Close      ar_ret     log_ret
+1 2005-09-27 19.30          NA          NA
+2 2005-09-28 20.50  0.06217617  0.06031979
+3 2005-09-29 20.21 -0.01414634 -0.01424735
+4 2005-09-30 21.01  0.03958436  0.03882098
+5 2005-10-03 21.50  0.02332223  0.02305442
+6 2005-10-04 22.16  0.03069767  0.03023593
+```
+Notice that first row has blank returns. The first record will be dropped in future calculations
+
+
+##### AAL
 ![AAL](aal_returns.svg)
 
-#### Reliance
+TODO Ali - write interpretation
+
+##### Reliance
 
 ![Reliance](rel_returns.svg)
 
-### Closer look on returns
+TODO Ali - write interpretation
+
+##### Closer look on returns
 
 ![Reliance](ret_zoom.svg)
 
+##### The Augmented Dickey-Fuller Test on Returns
 
+TODO Ali - write interpretation comparing values with ADF of `Close` above??
 
+```
+> # ADF test on ariphmetic return
+> print(adf.test(df.aal$ar_ret, alternative = "stationary"))
+
+	Augmented Dickey-Fuller Test
+
+data:  df.aal$ar_ret
+Dickey-Fuller = -13.646, Lag order = 15, p-value = 0.01
+alternative hypothesis: stationary
+
+Warning message:
+In adf.test(df.aal$ar_ret, alternative = "stationary") :
+  p-value smaller than printed p-value
+> # ADF test on log return
+> print(adf.test(df.aal$log_ret, alternative = "stationary"))
+
+	Augmented Dickey-Fuller Test
+
+data:  df.aal$log_ret
+Dickey-Fuller = -13.709, Lag order = 15, p-value = 0.01
+alternative hypothesis: stationary
+
+Warning message:
+In adf.test(df.aal$log_ret, alternative = "stationary") :
+  p-value smaller than printed p-value
+> # ADF test on return
+> print(adf.test(df.rel$ar_ret, alternative = "stationary"))
+
+	Augmented Dickey-Fuller Test
+
+data:  df.rel$ar_ret
+Dickey-Fuller = -18.868, Lag order = 17, p-value = 0.01
+alternative hypothesis: stationary
+
+Warning message:
+In adf.test(df.rel$ar_ret, alternative = "stationary") :
+  p-value smaller than printed p-value
+> print(adf.test(df.rel$log_ret, alternative = "stationary"))
+
+	Augmented Dickey-Fuller Test
+
+data:  df.rel$log_ret
+Dickey-Fuller = -18.717, Lag order = 17, p-value = 0.01
+alternative hypothesis: stationary
+
+Warning message:
+In adf.test(df.rel$log_ret, alternative = "stationary") :
+  p-value smaller than printed p-value
+```
+
+Conclusion: the Arithmetic and Log Returns hastime series is stationary properties thus allowing to apply ARIMA class models to forecast the time series.
--- methods/project/arima.R
+++ methods/project/arima.R
@@ -1,5 +1,4 @@
-ar_ret <- function(P) {c(NA, P[2:length(P)]/P[1:(length(P)-1)] -1)}
-log_ret <- function(P) {c(NA, log(P[2:length(P)]/P[1:(length(P)-1)]))}
+#0. read f1les
 airlaines.file <- 'data/stock_market_data-AAL.csv'
 rel.file <- 'data/RELIANCE.csv'
 
@@ -11,98 +10,136 @@ library(dplyr)
 df.aal <- df.aal %>% select(Date, Close)
 # AAL sorted backwards
 df.aal <- df.aal[order(df.aal$Date),]
+rownames(df.aal) <- NULL
+df.rel <- df.rel %>% select(Date,Close)
+df.rel <- df.rel[order(df.rel$Date),]
+rownames(df.rel) <- NULL
 
+library(ggplot2)
+# 1. Plot Close
+ggplot(df.aal, aes(df.aal$Date, y = Close, color = variable)) + 
+  geom_line(aes(y = df.aal$Close, col = "Close"))
+
+ggplot(df.rel, aes(df.rel$Date, y = Close, color = variable)) + 
+  geom_line(aes(y = df.rel$Close, col = "Close"))
+
+#2. ADF tests forp Close
+# https://en.wikipedia.org/wiki/Augmented_Dickey%E2%80%93Fuller_test
+library(tseries)
+# ADF test on Close AAL
+print(adf.test(df.aal$Close,alternative = "stationary"))
+# ADF test on Close REL
+print(adf.test(df.rel$Close,alternative = "stationary"))
+
+#3.
+#transform
+ar_ret <- function(P) {c(NA, P[2:length(P)]/P[1:(length(P)-1)] -1)}
+log_ret <- function(P) {c(NA, log(P[2:length(P)]/P[1:(length(P)-1)]))}
 # AAL returns
 df.aal$ar_ret <- ar_ret(df.aal$Close)
 df.aal$log_ret <- log_ret(df.aal$Close)
 df.aal <- na.omit(df.aal)
-
 # REL returns
-df.rel <- df.rel %>% select(Date, Close)
 df.rel$ar_ret <- ar_ret(df.rel$Close)
 df.rel$log_ret <- log_ret(df.rel$Close)
 df.rel <- na.omit(df.rel)
 
-
-library(tseries)
-# https://en.wikipedia.org/wiki/Augmented_Dickey%E2%80%93Fuller_test
-# ADF test on ariphmetic return
-print(adf.test(df.aal$ar_ret))
-# ADF test on log return
-print(adf.test(df.aal$log_ret))
-# ADF test on Close
-print(adf.test(df.aal$Close))
-library(ggplot2)
-# Plot Close
-ggplot(df.aal, aes(df.aal$Date, y = Close, color = variable)) + 
-  geom_line(aes(y = df.aal$Close, col = "Close"))
-# Plot Returns
+#4. Plot Returns
 ggplot(df.aal, aes(df.aal$Date, y = Returns, color = variable)) + 
   geom_line(aes(y = df.aal$ar_ret, col = "ar_ret")) +  
   geom_line(aes(y = df.aal$log_ret, col = "log_ret"))
-
-# ADF test on return
-print(adf.test(df.rel$ar_ret, alternative = "stationary"))
-print(adf.test(df.rel$log_ret))
-# ADF test on Close
-print(adf.test(df.rel$Close))
 #
-# Plot Close
-ggplot(df.rel, aes(df.rel$Date, y = Close, color = variable)) + 
-  geom_line(aes(y = df.rel$Close, col = "Close"))
 # Plot Returns
 ggplot(df.rel, aes(df.rel$Date, y = Returns, color = variable)) + 
   geom_line(aes(y = df.rel$ar_ret, col = "ar_ret")) +  
   geom_line(aes(y = df.rel$log_ret, col = "log_ret"))
 # Zoom - shoe 100 
-df100 <- df.aal[99:200,]
+df100 <- df.aal[99:130,]
 ggplot(df100, aes(df100$Date, y = Returns, color = variable)) + 
   geom_line(aes(y = df100$ar_ret, col = "ar_ret")) +  
   geom_line(aes(y = df100$log_ret, col = "log_ret"))
 
-Acf(df.rel$ar_ret)
-Pacf(df.rel$log_ret)
+
+# ACF-PACF of AR of AAL
+acf(df.aal$ar_ret)
+pacf(df.aal$ar_ret)
+# ACF-PACF of LOGR of AAL
+acf(df.aal$log_ret)
+pacf(df.aal$log_ret)
+# ACF PACF of REL
+acf(df.rel$ar_ret)
+pacf(df.rel$ar_ret)
+acf(df.rel$log_ret)
+pacf(df.rel$log_ret)
+#5. ADF tests on Ret
+# ADF test on ariphmetic return
+print(adf.test(df.aal$ar_ret, alternative = "stationary"))
+# ADF test on log return
+print(adf.test(df.aal$log_ret, alternative = "stationary"))
+# ADF test on return
+print(adf.test(df.rel$ar_ret, alternative = "stationary"))
+print(adf.test(df.rel$log_ret, alternative = "stationary"))
+
+
+library(stats)
+library(forecast)
+#
+aal.ar.autoarima <- auto.arima(df.aal[1:3000,]$ar_ret,
+                               max.order=300,
+                               trace = TRUE)
+aal.ar.autoarima.log <- auto.arima(df.aal[1:3000,]$log_ret,
+                               max.order=300,
+                               trace = TRUE)
+
+print(aal.ar.autoarima)
+print(aal.ar.autoarima.log)
+
+tsdisplay(residuals(aal.ar.autoarima), lag.max=15)
+tsdisplay(residuals(aal.ar.autoarima.log), lag.max=15)
+print(summary(aal.ar.autoarima))
+print(summary(aal.ar.autoarima.log))
+
+test_f <- forecast(aal.ar.autoarima,h=14)
+checkresiduals(test_f)#aal.ar.autoarima)
+plot(test_f, include= 30,
+     ylab = 'Ariphmetic Returns', 
+     xlab = 'Day', 
+     main = 'Arithmetic Returns: Actual vs Prodicted')
+lines(df.aal$ar_ret)
+
+test_f_log <- forecast(aal.ar.autoarima.log,h=14)
+checkresiduals(test_f_log)#aal.ar.autoarima)
+plot(test_f_log, include= 30,
+     ylab = 'Log Returns', 
+     xlab = 'Day', 
+     main = 'Log Returns: Actual vs Prodicted')
+lines(df.aal$log_ret)
+#
+# Fit REL
+rel.ar.autoarima <- auto.arima(df.rel[1:5500,]$ar_ret,
+                               stepwise=FALSE,
+                               parallel = TRUE, 
+                               num.cores = 4,
+                               max.order=300)
+print(rel.ar.autoarima)
+plot(forecast(rel.ar.autoarima,h=10),include = 30)
 
 #AAL Training set
 df.aal.train <- head(df.aal,3000)
 #AAL Testinng set
 df.aal.test <- tail(df.aal,length(df.aal)-3000)
 
-library(stats)
-aal.arima.tr <- arima(x = df.aal.train$ar_ret, order = c(2,0,2) )
+aal.model <- Arima(df.aal.train$ar_ret,order=c(3,0,3))
+plot(forecast(aal.model,h=14), include=28)
+#lines(df.aal.test$ar_ret)
 
-library(forecast)
+# Apply fitted model to later data
+aal.model2 <- Arima(df.aal.test$ar_ret,model=aal.model)
 
-aal.ar.autoarima <- auto.arima(df.aal.train$ar_ret,
-                               xreg = ,
-                               max.p = 30, 
-                               max.q = 30, 
-                               max.d = 3,
-                               trace = TRUE)
-print(aal.ar.autoarima)
-
-tsdisplay(residuals(aal.ar.autoarima), lag.max=15)
-accuracy(aal.ar.autoarima)
-print(summary(aal.ar.autoarima))
-test_f <- forecast(aal.ar.autoarima,h=5)
-checkresiduals(test_f)#aal.ar.autoarima)
-plot(test_f)
-library(TSPred)
-plotarimapred(df.aal, 
-              aal.ar.autoarima, 
-              xlim=c(3000,3392),
-              range.percent = 0.2,
-              ylab = 'Returns', 
-              xlab = 'Date', 
-              main = 'Returns: Actual vs Prodicted')
-
-# Fit REL
-rel.ar.autoarima <- auto.arima(df.rel$ar_ret,
-                               stepwise=FALSE,
-                               parallel = TRUE, 
-                               num.cores = 4,
-                               max.order=300)
-print(rel.ar.autoarima)
-plot(forecast(rel.ar.autoarima,h=4))
-
-
+# Forecast accuracy measures on the log scale.
+# in-sample one-step forecasts.
+accuracy(aal.model)
+# out-of-sample one-step forecasts.
+accuracy(aal.model2)
+# out-of-sample multi-step forecasts
+accuracy(forecast(aal.model,h=14,lambda=NULL),df.aal.test$ar_ret)
