a46835a7e4a7c06f49ed04fb16706ce4d663dbdf
Igor O'Sten <borodark@gmail.com>
2019-05-21 22:46:51 -0400

    cpp ass1:q1

--- csc5050/src/assignment1.cpp
+++ csc5050/src/assignment1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cassert>
 
 using namespace std;
 
@@ -12,6 +13,9 @@ public:
      last_name(ln),
      monthly_salary(ms)
   {
+    if (monthly_salary < 0){
+      monthly_salary = 0;
+    }
   }
   string getFirstName() const {
     return first_name;
@@ -40,10 +44,27 @@ private:
 };
 
 
+void q1(){
+  Employee emp1{"Igor", "Osten", 200000};
+  cout << "Welcome, the new emp1, making staggering " << emp1.getMonthlySalary() << "!" << endl;
+  assert(emp1.getFirstName() == "Igor");
+  Employee emp2{"Igor", "Osten1", -1000};
+  cout << "Welcome, the new emp2, making staggering " << emp2.getMonthlySalary() << "!" << endl;
+  assert(emp2.getMonthlySalary() == 0);
+  emp2.setMonthlySalary(1000000);
+  cout << "HR mistake is fixed! emp2 is now making " << emp2.getMonthlySalary() << "!" << endl;
+  cout << "Promotions are coming!" << endl;
+  emp1.setMonthlySalary(emp1.getMonthlySalary() * 1.1);
+  assert(emp1.getMonthlySalary() == 220000);
+  emp2.setMonthlySalary(emp2.getMonthlySalary() * 1.1);
+  assert(emp2.getMonthlySalary() == 1100000);
+  cout << "The emp1 is now making " << emp1.getMonthlySalary() << "!" << endl;
+  cout << "The emp2 is now making " << emp2.getMonthlySalary() << "!" << endl;
+
+}
+
 int main(){
-  std::cout << "HAHAHA";
-  Employee emp1{"Igor", "Osten", 220000};
-  cout << "Welcome, the new emp1 \n" << emp1.getMonthlySalary() << "!" << endl;
+  q1();
   return 0;
 }
 
