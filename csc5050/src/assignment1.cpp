e1d903e1bf69c906b639db22b7144a38d3a843dd
Igor O'Sten <borodark@gmail.com>
2019-05-23 20:35:36 -0400

    q4

--- csc5050/src/assignment1.cpp
+++ csc5050/src/assignment1.cpp
@@ -22,6 +22,27 @@ int read_number(){
   return rc;
 }
 
+void q4(){
+  cout << endl;
+  cout << "+ q4 ++++++++++" << endl;
+  cout << "Please enter integer one" << endl;
+  int i1 = read_number();
+  cout << "Please enter integer two or 0 to exit" << endl;
+  int i2 = read_number();
+  while (i2 != 0){
+    if ((i1 % i2) == 0){
+      cout << "the first is a multiple of the second!" << endl;
+    } else {
+      cout << "the first is NOT a multiple of the second!" << endl;
+    }
+    cout << endl;
+    cout << "Please enter integer one" << endl;
+    i1 = read_number();
+    cout << "Please enter integer two or 0 to exit" << endl;
+    i2 = read_number();
+  }
+}
+
 void q1(){
   cout << endl;
   cout << "+ q1 ++++++++++" << endl;
@@ -96,8 +117,9 @@ void q2(){
 }
 
 int main(){
-  q1();
-  q2();
-  return 0;
+q1();
+q2();
+q4();
+return 0;
 }
 
