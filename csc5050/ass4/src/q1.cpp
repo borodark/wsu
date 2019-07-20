222e3b1eb73b34a1954d28f1cc07fa1b05f325f2
Igor O'Sten <borodark@gmail.com>
2019-07-20 19:21:22 -0400

    print works

--- csc5050/ass4/src/q1.cpp
+++ csc5050/ass4/src/q1.cpp
@@ -18,34 +18,58 @@ private:
   int disks;
 public:
   Towers(int disks);
-  vector<int>* a = new vector<int>();
-  vector<int>* b = new vector<int>();
-  vector<int>* c = new vector<int>();
+  vector<int> a = vector<int>();
+  vector<int> b = vector<int>();
+  vector<int> c = vector<int>();
   void print();
 };
 Towers::Towers(int ofDisks) : disks(ofDisks)
 {
   for(int i = 0; i < disks; i++){
     cout << "pushing i = " << i+1 << endl;
-    a->push_back(i+1);
+    a.push_back(i+1);
   }
+  for(int i = 0; i < disks; i++){
+    cout << "pushing i = " << disks - i << endl;
+    c.push_back(disks-(i));
+  }
+
 }// end default constructor
 void Towers::print()
 {
   for(int i = 0; i < disks ; i++){ // from the top
-
+    int a_cur = (a.empty()?0:(i>a.size()?0:a.at(i)));
+    int b_cur = (b.empty()?0:(i>b.size()?0:b.at(i)));
+    int c_cur = (c.empty()?0:(i>c.size()?0:c.at(i)));
+    // if j belongs to A [0..disks-1]
     for(int j = 0 ; j < disks * 3 + 2 ; j++){
-      // if j belongs to A [0..disks-1]
-      if ((j >= 0) && (j < disks - 1)){ // A
-        cout << "= "; // TODO print A
+      if ((j >= 0) && (j < disks)){ // A
+        if (a_cur > 0){
+          cout << "= ";
+          a_cur--;
+        } else {
+          cout << "  "; // print space
+        }
       }
       // if j belongs to B [disks..2*disks-1]
-      else if ((j >= disks) && (j < 2*disks - 1)){ // A
-        cout << "+ "; // TODO print B
+      else if ((j >= disks) && (j < 2*disks)){ // A
+        // cout << "+ "; // TODO print B
+        if (b_cur > 0){
+          cout << "= ";
+          b_cur--;
+        } else {
+          cout << "  "; // print space
+        }
       }
       // if j belongs to C [2*disks .. 3*disks -1]
-      else if ((j >= 2*disks) && (j < 3*disks - 1)){ // A
-        cout << "- "; //TODO print C
+      else if ((j >= 2*disks) && (j < 3*disks)){ // A
+        // cout << "- "; //TODO print C
+        if (c_cur > 0){
+          cout << "= ";
+          c_cur--;
+        } else {
+          cout << "  "; // print space
+        }
       }
       else
         cout << "  "; // print space
@@ -53,7 +77,6 @@ void Towers::print()
     cout << endl;
   }
   cout << endl;
-
 };
 
 
@@ -78,13 +101,13 @@ void solveTowers(unsigned char disksLeft,
 
 void q1(){
   Towers for2 = Towers{2};
-  cout << "for2 has "<< for2.a->at(0) << " at the top"<< endl;
+  cout << "for2 has "<< for2.a.at(0) << " at the top"<< endl;
   for2.print();
   cout << "Solving for 2" << endl;
   solveTowers(2, 'A','B','C');
 
   Towers for10 = Towers{10};
-  cout << "for10 has "<< for10.a->at(0) << " at the top"<< endl;
+  cout << "for10 has "<< for10.a.at(0) << " at the top"<< endl;
   for10.print();
   cout << "Solving for 3" << endl;
   solveTowers(3, 'A','B','C');
