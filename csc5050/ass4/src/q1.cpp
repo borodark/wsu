18b7dfa071e55cb329a7eae96cdaa93d8f7a2f00
Igor O'Sten <borodark@gmail.com>
2019-07-21 11:54:20 -0400

    WORKS but print is inverted

--- csc5050/ass4/src/q1.cpp
+++ csc5050/ass4/src/q1.cpp
@@ -21,23 +21,53 @@ public:
   vector<int> a = vector<int>();
   vector<int> b = vector<int>();
   vector<int> c = vector<int>();
+  void move(unsigned char from, unsigned char to);
   void print();
 };
 Towers::Towers(int ofDisks) : disks(ofDisks)
 {
-  for(int i = 0; i < disks; i++){
-    cout << "pushing i = " << i+1 << endl;
-    a.push_back(i+1);
+  cout << "pushing "<< disks << " disks ... " << endl;
+  for(int i = disks; i > 0; i--){
+    // for(int i = 0; i < disks; i++){
+    cout << "pushing i = " << i << endl;
+    a.push_back(i);
   }
-  for(int i = 0; i < disks; i++){
-    cout << "pushing i = " << disks - i << endl;
-    c.push_back(disks-(i));
-  }
-
 }// end default constructor
-void Towers::print()
+
+void Towers::move(unsigned char from, unsigned char to)
 {
-  for(int i = 0; i < disks ; i++){ // from the top
+  vector<int>* f;
+  switch(from) {
+  case 'a' :
+    f = &a;
+    break;       // and exits the switch
+  case 'b' :
+    f = &b;
+    break;
+  case 'c' :
+    f = &c;
+    break;
+  }
+  vector<int>* t;
+  switch(to) {
+  case 'a' :
+    t = &a;
+    break;       // and exits the switch
+  case 'b' :
+    t = &b;
+    break;
+  case 'c' :
+    t = &c;
+    break;
+  }
+  int value = f->back();
+  f->pop_back();
+  cout << "moving " << value << " from " << from <<" to " << to << endl;
+  t->push_back(value);
+}
+void Towers::print() // TODO this is ugly! there must be a recursive way 
+{
+  for(int i = 0;  i < disks ; i++){ // from the top
     int a_cur = (a.empty()?0:(i>a.size()?0:a.at(i)));
     int b_cur = (b.empty()?0:(i>b.size()?0:b.at(i)));
     int c_cur = (c.empty()?0:(i>c.size()?0:c.at(i)));
@@ -53,7 +83,6 @@ void Towers::print()
       }
       // if j belongs to B [disks..2*disks-1]
       else if ((j >= disks) && (j < 2*disks)){ // A
-        // cout << "+ "; // TODO print B
         if (b_cur > 0){
           cout << "= ";
           b_cur--;
@@ -63,7 +92,6 @@ void Towers::print()
       }
       // if j belongs to C [2*disks .. 3*disks -1]
       else if ((j >= 2*disks) && (j < 3*disks)){ // A
-        // cout << "- "; //TODO print C
         if (c_cur > 0){
           cout << "= ";
           c_cur--;
@@ -79,38 +107,35 @@ void Towers::print()
   cout << endl;
 };
 
-
-
 void solveTowers(unsigned char disksLeft,
                  unsigned char src,
                  unsigned char dest,
-                 unsigned char spare ){
+                 unsigned char spare,
+                 Towers* state){
   if (disksLeft == 1) {
     cout << "Moving a disk from " << src << " to "
          << dest << "." << endl;
+    state->move(src, dest);
   } else {
-    solveTowers(disksLeft - 1,src, spare, dest);
-    solveTowers(1,src, dest, spare);
-    solveTowers(disksLeft -1,spare, dest, src);
+    solveTowers(disksLeft - 1,src, spare, dest, state);
+    solveTowers(1,src, dest, spare, state);
+    solveTowers(disksLeft -1,spare, dest, src, state);
   }
 }
 
-
-
-
-
 void q1(){
+
   Towers for2 = Towers{2};
-  cout << "for2 has "<< for2.a.at(0) << " at the top"<< endl;
+  cout << "for2 has "<< for2.a.at(1) << " at the top"<< endl;
   for2.print();
   cout << "Solving for 2" << endl;
-  solveTowers(2, 'A','B','C');
+  solveTowers(2, 'a','b','c', &for2);
 
   Towers for10 = Towers{10};
-  cout << "for10 has "<< for10.a.at(0) << " at the top"<< endl;
+  // cout << "for10 has "<< for10.a.at(0) << " at the top"<< endl;
   for10.print();
-  cout << "Solving for 3" << endl;
-  solveTowers(3, 'A','B','C');
+  cout << "Solving for 10" << endl;
+  solveTowers(10, 'a','b','c', &for10);
 }
 
 int main(){
