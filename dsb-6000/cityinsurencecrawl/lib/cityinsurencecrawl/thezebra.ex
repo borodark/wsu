a2f391453960c10bfefdd4d8eee6a9121169d448
Igor O'Sten <borodark@gmail.com>
2019-11-08 00:37:23 -0500

    before final

--- dsb-6000/cityinsurencecrawl/lib/cityinsurencecrawl/thezebra.ex
+++ dsb-6000/cityinsurencecrawl/lib/cityinsurencecrawl/thezebra.ex
@@ -1,6 +1,6 @@
 defmodule TheZebra do
   @behaviour Crawly.Spider
-
+  # Crawly.Engine.start_spider(TheZebra) 
   require Logger
   @impl Crawly.Spider
   def base_url(), do: "https://www.thezebra.com/"
@@ -14,15 +14,46 @@ defmodule TheZebra do
     ]
   end
 
+  ## city_data |> Enum.map(&TheZebra.parse_rates/1) |> Enum.filter(fn %{"key" => value} -> not MapSet.member?(MapSet.new(["Credit Quality","Average Car Insurance Rate","Age","Average Rate","Marital Status/Gender"]),value); %{"value" => value} -> true  end)
+  ## {"td", [], ["City Average"]},
+  ## Type II page with horizontal tables
+  def parse_rates({"td", [], ["$" <> value]}) do
+    {rate_int, ""} =
+      value
+      |> String.replace(",", "")
+      |> Integer.parse()
+
+    %{"value" => rate_int}
+  end
+
+  def parse_rates({"td", [], [label]}) do
+    %{"key" => label}
+  end
+
+  def parse_rates({"td", [{"style", "width: 50%;"}], ["$" <> value]}) do
+    {rate_int, ""} =
+      value
+      |> String.replace(",", "")
+      |> Integer.parse()
+
+    %{"value" => rate_int}
+  end
+
+  def parse_rates({"td", [{"style", "width: 50%;"}], [label]}) do
+    %{"key" => label}
+  end
+
   def parse_rates({"li", [], [text]}) do
-    [rate_column,rate_value] =
+    [rate_column, rate_value] =
       text
       |> String.split(": $")
-    {rate_int,_} =
+
+    {rate_int, _} =
       rate_value
-      |> String.replace(",","")
-      |> Integer.parse
-    %{rate_column => rate_int} 
+      |> String.replace(",", "")
+      |> Integer.parse()
+
+    %{rate_column => rate_int}
   end
 
   def parse_rates({"li", [_], [_]}) do
@@ -35,15 +66,16 @@ defmodule TheZebra do
     city_url_tuples =
       response.body
       |> Floki.find("td a")
-      |> Enum.map(fn {"a", [{_,ref}],[city]} -> {city, ref} end)
+      |> Enum.map(fn {"a", [{_, ref}], [city]} -> {city, ref} end)
 
     cities =
       city_url_tuples
-      |> Enum.map(fn {city, _} -> city end )
+      |> Enum.map(fn {city, _} -> city end)
 
     urls =
       city_url_tuples
-      |> Enum.map(fn {_, url} -> url end )
+      |> Enum.map(fn {_, url} -> url end)
+
     # Converting URLs into Crawly requests
 
     requests =
@@ -51,30 +83,94 @@ defmodule TheZebra do
       |> Enum.map(&build_absolute_url/1)
       |> Enum.map(&Crawly.Utils.request_from_url/1)
 
-    city =
+    # TODO grab city for type II
+    # "How Much Does Car Insurance Cost in "<>"Anchorage"<>"?"
+    # "Car Insurance in "<>"Renton"
+    # "How Much Are Car Insurance Rates in "<>city<>"?"
+    h2z =
       response.body
-      |> Floki.find("h1.blog-headline")
-      |> Floki.text
+      |> Floki.find("h2")
+
+    city =
+      h2z
+      |> (fn
+            [] -> ""
+            [one | _] -> one
+          end).()
+      |> Floki.text()
+      |> parse_city
+
     # TODO parse city if subsrings found in cities
-    rates_map = Map.new
-    rates = # [of maps]
+    rates_map = Map.new()
+    # [of maps]
+    ## Type I https://www.thezebra.com/lafayette-la-car-insurance/
+    rates_I =
       response.body
       |> Floki.find("div.blog-content-block ul li")
       |> Enum.map(fn x -> parse_rates(x) end)
       |> Enum.filter(fn x -> x != :ok end)
-      |> Enum.reduce(rates_map,fn x, acc -> Map.merge(acc,x) end)
-    # 
+      |> Enum.reduce(rates_map, fn x, acc -> Map.merge(acc, x) end)
+    #
+    ## Type II https://www.thezebra.com/lafayette-la-car-insurance/
+    rates_II =
+      response.body
+      |> Floki.find("tr td")
+      |> Enum.map(fn x -> parse_rates(x) end)
+      |> Enum.filter(fn x -> filter_type_II(x) end)
+
     %Crawly.ParsedItem{
       :items => [
         %{
           city: city,
-          rates: rates
+          rates: rates_II ++ rates_I
         }
       ],
       :requests => requests
     }
   end
 
-  defp build_absolute_url(url), do: URI.merge(base_url(), url) |> to_string()
+  def filter_type_II(%{"key" => value}) do
+    not MapSet.member?(
+      MapSet.new([
+        "Credit Quality",
+        "Average Car Insurance Rate",
+        "Age",
+        "Average Rate",
+        "Marital Status/Gender",
+        "Incident or Violation"
+      ]),
+      value
+    )
+  end
 
+  def filter_type_II(%{"value" => _}), do: true
+
+  def parse_city("What's the Cost of Car Insurance in " <> a_city?),
+    do: a_city? |> String.trim_trailing("?")
+
+  def parse_city("How Much Does Auto Insurance Cost in " <> a_city?),
+    do: a_city? |> String.trim_trailing("?")
+
+  def parse_city("How Much Does Car Insurance Cost in " <> a_city?),
+    do: a_city? |> String.trim_trailing("?")
+
+  def parse_city("How Much Are Car Insurance Rates in " <> a_city?),
+    do: a_city? |> String.trim_trailing("?")
+
+  def parse_city("What's the Average Cost of Car Insurance in " <> a_city?),
+    do: a_city? |> String.trim_trailing("?")
+
+  def parse_city("How Much Does Providence, RI, Car Insurance Cost?"),
+    do: "Providence"
+
+  def parse_city("How much does car insurance cost in Lexington, Kentucky?"),
+    do: "Lexington"
+
+  def parse_city("Car Insurance in " <> city), do: city
+
+  def parse_city("Car insurance rates by city"), do: :ok
+
+  def parse_city(_), do: :ok
+
+  defp build_absolute_url(url), do: URI.merge(base_url(), url) |> to_string()
 end
