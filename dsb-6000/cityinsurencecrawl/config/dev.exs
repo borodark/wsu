9cb189bae833d557cdda72d7b714f92c85061e45
Igor O'Sten <borodark@gmail.com>
2019-11-08 13:44:49 -0500

    Type I works Type II togo

--- dsb-6000/cityinsurencecrawl/config/dev.exs
+++ dsb-6000/cityinsurencecrawl/config/dev.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
+config :logger, :console, format: "[$level] $message\n"
+
 config :crawly,
   # The path where items are stored
   base_store_path: "data",
--- dsb-6000/cityinsurencecrawl/lib/cityinsurencecrawl/thezebra.ex
+++ dsb-6000/cityinsurencecrawl/lib/cityinsurencecrawl/thezebra.ex
@@ -62,34 +62,40 @@ defmodule TheZebra do
 
   @impl Crawly.Spider
   def parse_item(response) do
-    # Extracting pagination urls
-    city_url_tuples =
+    city_urls =
       response.body
-      |> Floki.find("td a")
-      |> Enum.map(fn {"a", [{_, ref}], [city]} -> {city, ref} end)
+      |> Floki.find("tbody tr td a")
 
-    cities =
-      city_url_tuples
-      |> Enum.map(fn {city, _} -> city end)
+    Logger.debug("#{inspect(city_urls)}")
 
     urls =
-      city_url_tuples
-      |> Enum.map(fn {_, url} -> url end)
+      if city_urls == [] do
+        []
+      else
+        city_urls
+        |> Enum.map(fn {"a", [{_, ref}], [_city]} -> ref end)
+      end
 
     # Converting URLs into Crawly requests
+
+    Logger.debug("#{inspect(urls)}")
 
     requests =
       urls
       |> Enum.map(&build_absolute_url/1)
       |> Enum.map(&Crawly.Utils.request_from_url/1)
 
-    # TODO grab city for type II
-    # "How Much Does Car Insurance Cost in "<>"Anchorage"<>"?"
-    # "Car Insurance in "<>"Renton"
-    # "How Much Are Car Insurance Rates in "<>city<>"?"
+    # .blog-content-block h2 # Rockport
+    # .zip-content div h2 # Kansas
+    city_list = response.body |> Floki.find(".blog-content-block h2")
+    #   Logger.debug("City List: #{inspect(city_list)}")
+
     h2z =
-      response.body
-      |> Floki.find("h2")
+      if city_list == [] do
+        response.body |> Floki.find(".zip-content div h2")
+      else
+        city_list
+      end
 
     city =
       h2z
@@ -100,31 +106,56 @@ defmodule TheZebra do
       |> Floki.text()
       |> parse_city
 
+    Logger.debug("City: #{inspect(city)}")
+
     # TODO parse city if subsrings found in cities
     rates_map = Map.new()
     # [of maps]
-    ## Type I https://www.thezebra.com/lafayette-la-car-insurance/
-    rates_I =
+    rates_raw =
       response.body
-      |> Floki.find("div.blog-content-block ul li")
-      |> Enum.map(fn x -> parse_rates(x) end)
-      |> Enum.filter(fn x -> x != :ok end)
-      |> Enum.reduce(rates_map, fn x, acc -> Map.merge(acc, x) end)
+      |> Floki.find(".blog-content-block ul li")
+      ## Type I https://www.thezebra.com/lafayette-la-car-insurance/
+      |> Floki.filter_out(".social-icons-list-item")
 
-    #
-    ## Type II https://www.thezebra.com/lafayette-la-car-insurance/
-    rates_II =
-      response.body
-      |> Floki.find("tr td")
-      |> Enum.map(fn x -> parse_rates(x) end)
-      |> Enum.filter(fn x -> filter_type_II(x) end)
+#    Logger.debug("Rates Raw: #{inspect(rates_raw)}")
+
+    rates =
+      if rates_raw == [] do
+        response.body
+        ## Type II https://www.thezebra.com/anchorage-ak-car-insurance/
+        |> Floki.find(".col-lg-9 tr td") # .col-lg-9 tr td
+        |> Enum.map(fn x -> parse_rates(x) end)
+        |> Enum.filter(fn x -> filter_type_II(x) end)
+      else
+        ## we R on the first page with list of urls for the cityes
+        if city == :ok do
+          # we R on the first page with list of urls for the cityes
+          []
+        else
+          rates_raw
+          |> Enum.map(fn x -> parse_rates(x) end)
+          |> Enum.filter(fn x -> x != :ok end)
+          |> Enum.reduce(rates_map, fn x, acc -> Map.merge(acc, x) end)
+        end
+      end
+
+    Logger.debug("Rates: #{inspect(rates)}")
+
+    items =
+    if rates == [] do
+      %{
+        city: city
+      }
+    else
+      %{
+        city: city
+      } |> Map.merge(rates)
+    end
+
 
     %Crawly.ParsedItem{
       :items => [
-        %{
-          city: city
-        }
-        |> Map.merge(rates_I)
+      items
       ],
       :requests => requests
     }
