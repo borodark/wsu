c1c953e311e1e3b96abe8e123be8766977ae508e
Igor O'Sten <borodark@gmail.com>
2019-11-10 22:13:51 -0500

    donate

--- dsb-6000/cityinsurencecrawl/config/dev.exs
+++ dsb-6000/cityinsurencecrawl/config/dev.exs
@@ -5,6 +5,8 @@ config :logger, :console, format: "[$level] $message\n"
 config :crawly,
   # The path where items are stored
   base_store_path: "data",
+  output_format: "csv",
   pipelines: [
+    # Crawly.Pipelines.CSVEncoder
     Crawly.Pipelines.JSONEncoder
   ]
--- dsb-6000/cityinsurencecrawl/lib/cityinsurencecrawl/thezebra.ex
+++ dsb-6000/cityinsurencecrawl/lib/cityinsurencecrawl/thezebra.ex
@@ -14,6 +14,63 @@ defmodule TheZebra do
     ]
   end
 
+  def states_list do
+    [
+      "Alabama",
+      "Alaska",
+      "Arizona",
+      "Arkansas",
+      "California",
+      "Colorado",
+      "Connecticut",
+      "Delaware",
+      "Florida",
+      "Georgia",
+      "Hawaii",
+      "Idaho",
+      "Illinois",
+      "Indiana",
+      "Iowa",
+      "Kansas",
+      "Kentucky",
+      "Louisiana",
+      "Maine",
+      "Maryland",
+      "Massachusetts",
+      "Michigan",
+      "Minnesota",
+      "Mississippi",
+      "Missouri",
+      "Montana",
+      "Nebraska",
+      "Nevada",
+      "New Hampshire",
+      "New Jersey",
+      "New Mexico",
+      "New York",
+      "North Carolina",
+      "North Dakota",
+      "Ohio",
+      "Oklahoma",
+      "Oregon",
+      "Pennsylvania",
+      "Rhode Island",
+      "South Carolina",
+      "South Dakota",
+      "Tennessee",
+      "Texas",
+      "Utah",
+      "Vermont",
+      "Virginia",
+      "Washington",
+      "West Virginia",
+      "Wisconsin",
+      "Wyoming",
+      "Washington",
+      "D.C"
+    ]
+  end
+
   ## city_data |> Enum.map(&TheZebra.parse_rates/1) |> Enum.filter(fn %{"key" => value} -> not MapSet.member?(MapSet.new(["Credit Quality","Average Car Insurance Rate","Age","Average Rate","Marital Status/Gender"]),value); %{"value" => value} -> true  end)
   ## {"td", [], ["City Average"]},
   ## Type II page with horizontal tables
@@ -106,7 +163,46 @@ defmodule TheZebra do
       |> Floki.text()
       |> parse_city
 
-    Logger.debug("City: #{inspect(city)}")
+    Logger.debug("CITY IS: #{inspect(city)}")
+    # .zip-content.container.section-md  div  div p
+    # body div.container div.row.blog-content-wrap.justify-content-center div.col-md-10.col-lg-7.col-xxl-6.blog-content-block p
+    # body > main > section.zip-content.container.section-md > div > div > p:nth-child(3)
+    # "body main section.zip-content.container.section-md div.raw div.col-lg-9 p"
+    state_l =
+      response.body
+      |> Floki.find("body div p")
+      |> Floki.text()
+      |> String.split([" ", "'"])
+      |> Enum.filter(fn word -> Enum.member?(states_list(), word) end)
+      |> Enum.take(1)
+
+    # par_state =
+    #  if par_state == "" do
+    #  else
+    #    ""
+    #  end
+    state =
+      if state_l != [] do
+        [st | _] = state_l
+        st
+      else
+        ""
+      end
+
+    # body div.container div.row.blog-content-wrap.justify-content-center div.col-md-10.col-lg-7.col-xxl-6.blog-content-block div.rich-text p
+
+    Logger.debug("State paresd: #{inspect(state)} ")
+
+    city_state =
+      if state != "" do
+        if city != :ok do
+          city <> ", " <> state
+        end
+      else
+        city
+      end
+
+    Logger.debug("City,State: #{inspect(city_state)}")
 
     # TODO parse city if subsrings found in cities
     rates_map = Map.new()
@@ -117,33 +213,43 @@ defmodule TheZebra do
       ## Type I https://www.thezebra.com/lafayette-la-car-insurance/
       |> Floki.filter_out(".social-icons-list-item")
 
-#    Logger.debug("Rates Raw: #{inspect(rates_raw)}")
+    #    Logger.debug("Rates Raw: #{inspect(rates_raw)}")
 
     rates =
       if rates_raw == [] do
         rates_II =
-        response.body
-        ## Type II https://www.thezebra.com/anchorage-ak-car-insurance/
-        |> Floki.find(".col-lg-9 tr td") # .col-lg-9 tr td
-        |> Enum.map(fn x -> parse_rates(x) end)
-        |> Enum.filter(fn x -> filter_type_II(x) end)
+          response.body
+          ## Type II https://www.thezebra.com/anchorage-ak-car-insurance/
+          # .col-lg-9 tr td
+          |> Floki.find(".col-lg-9 tr td")
+          |> Enum.map(fn x -> parse_rates(x) end)
+          |> Enum.filter(fn x -> filter_type_II(x) end)
+
         # TODO
 
-        six = rates_II |> Enum.take(6)
-        Logger.debug("Six: #{inspect six}")
-        rates_rc  =
-        if six != [] do
-          [%{"key" => k1},
-           %{"key" => k2},
-           %{"key" => k3},
-           %{"value" => v1},
-           %{"value" => v2},
-           %{"value" => v3}] = six
-          %{k1 => v1, k2 => v2, k3 => v3}
-        else
-          %{}
-        end
-        rates_rc
+        {six, rest} = rates_II |> Enum.split(6)
+        Logger.debug("Six: #{inspect(six)}")
+
+        rates_rc =
+          if six != [] do
+            [
+              %{"key" => k1},
+              %{"key" => k2},
+              %{"key" => k3},
+              %{"value" => v1},
+              %{"value" => v2},
+              %{"value" => v3}
+            ] = six
+
+            %{k1 => v1, k2 => v2, k3 => v3}
+          else
+            %{}
+          end
+
+        Logger.debug("City is: #{inspect(city)}")
+
+        ## TODO %{"key" => "City Average"}, %{"value" => 2814}, %{"key" => "City Average"}, %{"value" => 2814}],
+        parse_kv(rates_rc, rest)
       else
         ## we R on the first page with list of urls for the cityes
         if city == :ok do
@@ -157,23 +263,27 @@ defmodule TheZebra do
         end
       end
 
-    Logger.debug("Rates: #{inspect(rates)}")
+    #    Logger.debug("Rates: #{inspect(rates)}")
 
     items =
-    if rates == [] do
-      %{
-        city: city
-      }
-    else
-      %{
-        city: city
-      } |> Map.merge(rates)
-    end
-
+      if rates == [] do
+        %{
+          "city_comma_state" => city_state,
+          "city" => city,
+          "state" => state
+        }
+      else
+        %{
+          "city_comma_state" => city_state,
+          "city" => city,
+          "state" => state
+        }
+        |> Map.merge(rates)
+      end
 
     %Crawly.ParsedItem{
       :items => [
-      items
+        items
       ],
       :requests => requests
     }
@@ -182,12 +292,22 @@ defmodule TheZebra do
   def filter_type_II(%{"key" => value}) do
     not MapSet.member?(
       MapSet.new([
+        "Credit",
+        "Credit Score",
         "Credit Quality",
+        "Credit Level",
         "Average Car Insurance Rate",
         "Age",
         "Average Rate",
+        "Mean Rate",
+        "Marital Status",
         "Marital Status/Gender",
-        "Incident or Violation"
+        "Violation/Incident",
+        "Incident/Violation",
+        "Incident or Violation",
+        "Average Insurance Rate",
+        "Average Annual Premium",
+        "Average Auto Insurance Rate"
       ]),
       value
     )
@@ -221,6 +341,16 @@ defmodule TheZebra do
   def parse_city("Car insurance rates by city"), do: :ok
 
   def parse_city(_), do: :ok
+
+  ## take pairs of  %{"key" => "City Average"}, %{"value" => 2814}
+  def parse_kv(acc, [key | [value | rest]]) do
+    %{"key" => k} = key
+    %{"value" => v} = value
+    #    Logger.debug("#{inspect(rest)}")
+    parse_kv(acc |> Map.merge(%{k => v}), rest)
+  end
+
+  def parse_kv(acc, []), do: acc
 
   defp build_absolute_url(url), do: URI.merge(base_url(), url) |> to_string()
 end
